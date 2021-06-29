3b089f17d783676885474971257973b63ca209d6
leocassiosilva <leocassiosilva1234@gmail.com>
2021-06-29 09:22:38 -0300

    front

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.body}>
+      <View style={styles.container}>
+        <Text style={styles.text_container}>0.0</Text>
+
+        <View style={styles.btn_container}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btn_container_text}>Iniciar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity  style={styles.btn}>
+            <Text style={styles.btn_container_text}>Resetar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#1B365C',
     justifyContent: 'center',
+  },
+
+  container: {
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+
+  btn_container:{
+    flexDirection:"row",
+    padding:20,
+  },
+
+ btn:{
+    marginHorizontal:20,
+    backgroundColor:'#7EA1C4',
+    height:50,
+    width:75,
+    alignItems:'center', 
+    justifyContent:'center',
+    borderRadius:5
+ },
+
+
+
+  text_container: {
+    color: 'white',
+    fontSize: 35,
+    textAlign:'center'
+  },
+
+  btn_container_text:{
+    color: 'white',
+    fontSize: 15,
+    textAlign:'center'
   },
 });
